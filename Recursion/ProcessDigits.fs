1a27ef8aa2bcf147fe22060170c10b3194e382ab
MoiBeTeroK <ludarikkot@gmail.com>
2025-04-05 02:15:38 +0300

    L5 task9 add function to process digits with condition

--- Recursion/ProcessDigits.fs
+++ Recursion/ProcessDigits.fs
@@ -4,3 +4,13 @@ let rec processDigits num (func: int->int->int) acc =
     match num with
     | 0 -> acc
     | _ -> processDigits (num / 10) func (func acc (num % 10))
+
+
+let rec processDigitsCondition num (func: int->int->int) acc (condition: int->bool) =
+    match num with 
+    | 0 -> acc
+    | _ ->
+        let trueFalse = condition (num % 10)
+        match trueFalse with
+        | true -> processDigitsCondition (num / 10) func (func acc (num % 10)) condition
+        | false -> processDigitsCondition (num / 10) func acc condition
