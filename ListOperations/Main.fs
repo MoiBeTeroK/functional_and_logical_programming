765f5c47ce078b9450e8cde78a49547bc1e3c9c0
MoiBeTeroK <ludarikkot@gmail.com>
2025-04-09 15:12:09 +0300

    L6 Task12 add functions to reverse elements between min and max values in list

--- ListOperations/Main.fs
+++ ListOperations/Main.fs
@@ -4,14 +4,19 @@ open NumberTasks
 [<EntryPoint>]
 let main argv =
     // 11 задание
-    let lst = [5; 3; 8; 1; 4]
-    let minIndexList = indexOfMinList lst
-    Console.WriteLine("Индекс минимального элемента (возможности класса List): {0}", minIndexList)
+    // let lst = [5; 3; 8; 1; 4]
+    // let minIndexList = indexOfMinList lst
+    // Console.WriteLine("Индекс минимального элемента (возможности класса List): {0}", minIndexList)
 
-    let minIndexChurch = indexOfMinTailRec lst
-    Console.WriteLine("Индекс минимального элемента (списки Черча): {0}", minIndexChurch)
+    // let minIndexChurch = indexOfMinTailRec lst
+    // Console.WriteLine("Индекс минимального элемента (списки Черча): {0}", minIndexChurch)
 
     // 12 задания
+    let lst = [1; 3; 5; 7; 9]
+    let valuesReverseBetweenMinMax = reverseBetweenMinMax lst
+    let valuesReverseBetweenMinMaxChurch = reverseBetweenMinMaxChurch lst
+    printfn "Массив с элементами в обратном порядке между min и max (возможности класса List): %A" valuesReverseBetweenMinMax
+    printfn "Массив с элементами в обратном порядке между min и max (списки Черча): %A" valuesReverseBetweenMinMaxChurch
 
     // 13 задания
 
--- ListOperations/NumberTasks.fs
+++ ListOperations/NumberTasks.fs
@@ -1,10 +1,10 @@
 module NumberTasks
 // Вариант 2, задания 2, 12, 22, 32, 42, 52
 
-// 11.2.1 Дан целочисленный массив. Необходимо найти индекс минимального элемента.
+// 1.2.1 Дан целочисленный массив. Необходимо найти индекс минимального элемента.
 let indexOfMinList lst = lst |> List.mapi (fun i x -> (i, x)) |> List.minBy snd |> fst
 
-// 11.2.2
+// 1.2.2
 let indexOfMinTailRec lst =
     let rec loop tailItems currentIndex minValue minIndex =
         match tailItems with
@@ -18,3 +18,54 @@ let indexOfMinTailRec lst =
     | [] -> failwith "Пустой список"
     | x :: tail -> loop tail 1 x 0
 
+// 1.12.1 Дан целочисленный массив. Необходимо переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами.
+let reverseBetweenMinMax (lst: int list) : int list =
+    if List.length lst <= 3 then lst
+    else
+        let minElem = List.min lst
+        let maxElem = List.max lst
+
+        let minIndex = List.findIndex (fun x -> x = minElem) lst
+        let maxIndex = List.findIndex (fun x -> x = maxElem) lst
+
+        let start, finish = 
+            if minIndex < maxIndex then minIndex, maxIndex
+            else maxIndex, minIndex
+
+        let before = List.take (start + 1) lst
+        let toReverse = lst |> List.skip (start + 1) |> List.take (finish - start - 1) |> List.rev
+        let after = List.skip finish lst
+
+        List.concat [before; toReverse; after]
+
+// 1.12.2
+let indexOfMaxTailRec lst =
+    let rec loop tailItems currentIndex maxValue maxIndex =
+        match tailItems with
+        | [] -> maxIndex
+        | x :: rest ->
+            if x > maxValue then
+                loop rest (currentIndex + 1) x currentIndex
+            else
+                loop rest (currentIndex + 1) maxValue maxIndex
+    match lst with
+    | [] -> failwith "Пустой список"
+    | x :: tailItems -> loop tailItems 1 x 0
+
+let reverseBetweenMinMaxChurch list =
+    let minIndex = indexOfMinTailRec list
+    let maxIndex = indexOfMaxTailRec list
+    let start, finish = 
+            if minIndex < maxIndex then minIndex, maxIndex
+            else maxIndex, minIndex
+    let rec loop currentIndex tailItems res =
+        match tailItems with
+        | [] -> List.rev res
+        | x::tail ->
+            if currentIndex > start && currentIndex < finish then
+                let mirroredPos = finish - (currentIndex - start)
+                let mirroredValue = list.[mirroredPos]
+                loop (currentIndex+1) tail (mirroredValue::res)
+            else
+                loop (currentIndex+1) tail (x::res)
+    loop 0 list []
