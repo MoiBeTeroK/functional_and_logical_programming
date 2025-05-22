f95dec68f516bea19ae095dd11ad5e90efbd58a8
MoiBeTeroK <ludarikkot@gmail.com>
2025-05-23 00:16:36 +0300

    Some fix

--- F#/IndividualTask/MinimumAmountXYZ.fs
+++ F#/IndividualTask/MinimumAmountXYZ.fs
@@ -1,50 +1,48 @@
-// 142.Нужно найти минимальную сумму x + y + z с целыми числами x > y > z > 0 такой, что x + y, x - y, x + z, x - z, y + z, y - z являются идеальными квадратами.
 open System
-
-let isSquare n =
+let isPerfectSquare n =
     let root = int (sqrt (float n))
     root * root = n
 
-let squares = [1 .. 1000] |> List.map (fun x -> x * x)
+let trySolution a c e =
+    let aa = a * a
+    let cc = c * c
+    let ee = e * e
 
-let allPairs =
-    squares
-    |> List.collect (fun a ->
-        squares
-        |> List.filter (fun b -> a > b)
-        |> List.map (fun b -> (a + b, (a, b))))
+    let ff = aa - cc
+    let bb = cc - ee
+    let dd = bb + ff
 
-let groupedPairs =
-    allPairs
-    |> List.groupBy fst
-    |> List.map (fun (key, lst) -> (key, List.map snd lst))
+    if isPerfectSquare ff && isPerfectSquare bb && isPerfectSquare dd then 
+        let x2 = cc + dd
+        let y2 = ee + ff
+        let z2 = cc - dd
 
-let findFirstTripleAndSum =
-    groupedPairs
-    |> List.tryPick (fun (_, pairs) ->
-        pairs
-        |> List.tryPick (fun (a, b) ->
-            let x = (a + b) / 2
-            let y = (a - b) / 2
-            if y > 0 && x > y then
-                pairs
-                |> List.tryPick (fun (c, d) ->
-                    let z = (c - d) / 2
-                    if z > 0 && (c + d) / 2 = x && y > z && isSquare (y + z) && isSquare (y - z) then
-                        Some ((x, y, z), x + y + z)
-                    else
-                        None
-                )
-            else
-                None
-        )
-    )
-    
+        if x2 % 2 = 0 && y2 % 2 = 0 && z2 % 2 = 0 then
+            let x = x2 / 2
+            let y = y2 / 2
+            let z = z2 / 2
+
+            if z > 0 && x > y && y > z then
+                Some (x, y, z)
+            else None
+        else None
+    else None
+
+let rec generateCombinations a =
+    seq {1..a-1} |> Seq.collect (fun c -> seq {1..c-1} |> Seq.map (fun e -> (a, c, e)))
+    |> Seq.append <| seq {
+        yield! generateCombinations (a + 1)
+    }
+
+let findFirstSolution () = generateCombinations 2|> Seq.tryPick (fun (a, c, e) -> trySolution a c e)
+
+let printSolution (x, y, z) =
+    printfn "x = %d y = %d z = %d" x y z
+    printfn "x + y + z = %d" (x + y + z)
+
 [<EntryPoint>]
 let main argv =
-    match findFirstTripleAndSum with
-        | Some ((x, y, z), totalSum) ->
-            printfn "Найдена тройка: x = %d, y = %d, z = %d, сумма = %d" x y z totalSum
-        | None ->
-            printfn "Ничего не найдено"
+    match findFirstSolution () with
+    | Some solution -> printSolution solution
+    | None -> printfn "No solution found"
     0
